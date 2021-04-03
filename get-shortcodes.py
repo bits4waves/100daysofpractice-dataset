513817118dad219f29e47e5df329ad0841b0d71b
eglur <eglur@example.com>
2021-04-03 20:02:06 -0300

    Cron automatic backup

--- get-shortcodes.py
+++ get-shortcodes.py
@@ -3,12 +3,14 @@ import time
 import os
 
 I = instaloader.Instaloader()
-I.interactive_login(os.getenv("IG_USER"))
-query = instaloader.Hashtag.from_name(I.context, "100daysofpractice")
+I.interactive_login(os.getenv('IG_USER'))
+query = instaloader.Hashtag.from_name(I.context, '100daysofpractice')
 k = 1
 for post in query.get_all_posts():
     print(k)
-    with open("shortcodes.txt", "a") as file_object:
-        file_object.write(post.shortcode + "\n")
+    shortcode = post.shortcode
+    print(shortcode)
+    with open('shortcodes.txt', 'a') as file_object:
+        file_object.write(shortcode + '\n')
     time.sleep(1)
     k += 1
