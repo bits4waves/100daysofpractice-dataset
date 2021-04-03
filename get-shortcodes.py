55642d2c63e52f7fd7314560846a1113ac767cf3
eglur <eglur@example.com>
2021-04-03 19:52:15 -0300

    Cron automatic backup

--- get-shortcodes.py
+++ get-shortcodes.py
@@ -3,7 +3,7 @@ import time
 import os
 
 I = instaloader.Instaloader()
-I.interactive_login(os.getenv('IG_USER'))
+I.interactive_login(os.getenv("IG_USER"))
 query = instaloader.Hashtag.from_name(I.context, "100daysofpractice")
 k = 1
 for post in query.get_all_posts():
