345ddd5e3f2faf7289461387bd8ea345fcea5996
eglur <eglur@example.com>
2021-04-07 18:30:13 -0300

    work directly on the shell script

--- src/get-usernames.sh
+++ src/get-usernames.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+SHORTCODES=~/sci/100daysofpractice-dataset/shortcodes/shortcodes-test.txt
+PYTHON=~/sci/100daysofpractice-dataset/venv/bin/python
+OUTPUT=shortcode-username.csv
+
+while read SHORTCODE; do
+    USERNAME=$($PYTHON get-username.py $SHORTCODE)
+    PAIR=$SHORTCODE,$USERNAME
+    echo $PAIR
+    echo $PAIR >> shortcode-username.csv
+done <$SHORTCODES
