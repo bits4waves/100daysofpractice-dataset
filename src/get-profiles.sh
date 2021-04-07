2d98e27476be449232abb0adcadc56f9bff6f6cf
eglur <eglur@example.com>
2021-04-07 18:41:17 -0300

    Cron automatic backup

--- src/get-profiles.sh
+++ src/get-profiles.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-SHORTCODES=~/sci/100daysofpractice-dataset/shortcodes/shortcodes-test.txt
-PYTHON=~/sci/100daysofpractice-dataset/venv/bin/python
-CSVFILE=shortcode-username.csv
+PROJECT=~/sci/100daysofpractice-datase
+PYTHON=$PROJECT/venv/bin/python
+SRC=$PROJECT/src
+GET_USERNAME="$PYTHON $SRC/get-username.py"
+SHORTCODES=$PROJECT/shortcodes/shortcodes-test.txt
+PROFILES=$PROJECT/profiles
+CSV=$PROFILES/shortcode-username.csv
 
 while read SHORTCODE; do
-    USERNAME=$($PYTHON get-username.py $SHORTCODE)
+    USERNAME=$($GET_USERNAME $SHORTCODE)
     PAIR=$SHORTCODE,$USERNAME
     echo $PAIR
     echo $PAIR >> $CSVFILE
+    instaloader $USERNAME
 done <$SHORTCODES
