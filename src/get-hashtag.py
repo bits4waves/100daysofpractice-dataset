f15fbd93f84d5f8bd8a382124b2ff7597a03e296
eglur <eglur@example.com>
2021-04-12 17:46:45 -0300

    add get-hashtag.ph
    code from:
    https://github.com/instaloader/instaloader/issues/1077#issuecomment-817962327

---
+++ src/get-hashtag.py
@@ -0,0 +1,35 @@
+import instaloader
+
+SHORTCODE_FILE = "shortcodes.txt"
+HASHTAG = "100daysofpractice"
+IG_USER = ... # (insert your username)
+
+L = instaloader.Instaloader(fatal_status_codes=[400, 429])
+L.load_session_from_file(IG_USER)
+
+# Resumable iterator for recent hashtag posts, from #874.
+hashtag_posts = instaloader.NodeIterator(
+    context=L.context,
+    query_hash="9b498c08113f1e09617a1703c22b2f32",
+    edge_extractor=lambda d: d["data"]["hashtag"]["edge_hashtag_to_media"],
+    node_wrapper=lambda n: instaloader.Post(L.context, n),
+    query_variables={"tag_name": HASHTAG},
+    query_referer=f"https://www.instagram.com/explore/tags/{HASHTAG}/",
+)
+
+with open(SHORTCODE_FILE, "a") as file:
+    with instaloader.resumable_iteration(
+            context=L.context,
+            iterator=hashtag_posts,
+            load=instaloader.load_structure_from_file,
+            save=instaloader.save_structure_to_file,
+            format_path=lambda magic: f"resume_info_{magic}.json.xz",
+    ) as (is_resuming, start_index):
+        if is_resuming:
+            # After resuming, the first post is the last post that had been returned in
+            # the previous iteration. Here we avoid that this post shortcode is
+            # repeated.
+            next(hashtag_posts)
+            # Here we write all shortcodes to the file
+        for post in hashtag_posts:
+            print(post.shortcode, file=file)
