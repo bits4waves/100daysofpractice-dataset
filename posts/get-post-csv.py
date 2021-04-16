7b8f299426acbf2aa8321e654e024e52d18d3e3d
eglur <eglur@example.com>
2021-04-16 18:33:36 -0300

    add dict fields for extraction

--- posts/get-post-csv.py
+++ posts/get-post-csv.py
@@ -1,15 +1,29 @@
 import json
 import csv
+import re
 
-POSTS_JSON = "posts.json"
-POSTS_CSV = "posts.csv"
-with open(POSTS_JSON, "r") as postsjson, open(POSTS_CSV, "w") as postscsv:
-    atfirstline = True
-    for linejson in postsjson:
-        postdict = json.loads(linejson)['_node']
-        csvwriter = csv.DictWriter(postscsv, postdict.keys())
-        if atfirstline:
-            csvwriter.writeheader()
-            atfirstline = False
-        csvwriter.writerow(postdict)
+JSON_FILE = 'posts-test.json'
+CSV_FILE = 'posts-test.csv'
+with open(JSON_FILE, 'r', newline='') as json_file, open(CSV_FILE, 'w', newline='') as csv_file:
+    at_firstline = True
+    for line in json_file:
+        json_dict = json.loads(line)['_node']
+        csv_dict = dict()
+        csv_dict['id'] = json_dict['id']
+        csv_dict['shortcode'] = json_dict['shortcode']
+        csv_dict['taken_at_timestamp'] = json_dict['taken_at_timestamp']
+        csv_dict['owner-id'] = json_dict['owner']['id']
+        csv_dict['is_video'] = json_dict['is_video']
+        csv_dict['edge_liked_by-count'] = json_dict['edge_liked_by']['count']
+        csv_dict['edge_media_to_comment-count'] = json_dict['edge_media_to_comment']['count']
+        csv_dict['video_view_count'] = json_dict['video_view_count']
+        csv_dict['comments_disabled'] = json_dict['comments_disabled']
+        csv_dict['__typename'] = json_dict['__typename']
+        csv_dict['comments_disabled'] = json_dict['comments_disabled']
+        csv_writer = csv.DictWriter(csv_file, csv_dict.keys())
 
+        if at_firstline:
+            csv_writer.writeheader()
+            at_firstline = False
+
+        csv_writer.writerow(csv_dict)
