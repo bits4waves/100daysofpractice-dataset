8b1737b175e8182fdd75527c9e6c0cf1e52463de
eglur <eglur@example.com>
2021-04-16 18:49:56 -0300

    use helper function

--- posts/get-post-csv.py
+++ posts/get-post-csv.py
@@ -22,7 +22,7 @@ with open(JSON_FILE, 'r', newline='') as json_file, open(CSV_FILE, 'w', newline=
         csv_dict['is_video'] = json_dict['is_video']
         csv_dict['edge_liked_by-count'] = json_dict['edge_liked_by']['count']
         csv_dict['edge_media_to_comment-count'] = json_dict['edge_media_to_comment']['count']
-        csv_dict['video_view_count'] = json_dict['video_view_count']
+        csv_dict['video_view_count'] = get_video_count(json_dict)
         csv_dict['comments_disabled'] = json_dict['comments_disabled']
         csv_dict['__typename'] = json_dict['__typename']
         csv_dict['comments_disabled'] = json_dict['comments_disabled']
